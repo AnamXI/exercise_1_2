5a332ba0d2d197dfaa76619a7ac1d088a26e12d5
AnamXI <anamggss1227@gmail.com>
2023-07-12 11:08:31 +0800

    minor update

--- Assets/Dungeon Knight/Scripts/Playermovement.cs
+++ Assets/Dungeon Knight/Scripts/Playermovement.cs
@@ -11,6 +11,8 @@ public class Playermovement : MonoBehaviour
     public Vector2 movementInput;
     public Rigidbody2D rigidBody;
     public Animator anim;
+    public int coinCounter;
+    public int hp;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,49 +23,63 @@ public class Playermovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
 
-            anim.enabled = true;
-            anim.SetTrigger("PForwardAnim" );
-        }
+        //    anim.enabled = true;
+        //    anim.SetTrigger("PForwardAnim" );
+        //}
 
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            anim.enabled = false;
-        }
+        //if (Input.GetKeyUp(KeyCode.S))
+        //{
+        //    anim.enabled = false;
+        //}
 
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            anim.enabled = true;
-            anim.SetTrigger("PBackwardAnim");
-        }
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    anim.enabled = true;
+        //    anim.SetTrigger("PBackwardAnim");
+        //}
 
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            anim.enabled = false;
-        }
+        //if (Input.GetKeyUp(KeyCode.W))
+        //{
+        //    anim.enabled = false;
+        //}
 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            anim.enabled = true;
-            anim.SetTrigger("PLeftAnim");
-        }
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    anim.enabled = true;
+        //    anim.SetTrigger("PLeftAnim");
+        //}
 
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            anim.enabled = false;
-        }
+        //if (Input.GetKeyUp(KeyCode.A))
+        //{
+        //    anim.enabled = false;
+        //}
 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            anim.enabled = true;
-            anim.SetTrigger("PRightAnim");
-        }
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    anim.enabled = true;
+        //    anim.SetTrigger("PRightAnim");
+        //}
 
-        if (Input.GetKeyUp(KeyCode.D))
+        //if (Input.GetKeyUp(KeyCode.D))
+        //{
+        //    anim.enabled = false;
+        //}
+
+        anim.SetFloat("Horizontal", movementInput.x);
+        anim.SetFloat("Vertical", movementInput.y);
+        anim.SetFloat("Speed", movementInput.sqrMagnitude);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Coins"))
         {
-            anim.enabled = false;
+            coinCounter++;
+            Destroy(collision.gameObject);
         }
     }
 
